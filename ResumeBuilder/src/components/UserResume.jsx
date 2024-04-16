a83759e01240174c55ed430bfde3771d5485095c
Athul Bhandary <athulbhandary28@gmail.com>
2024-04-16 22:50:58 +0530

    Added download resume button

--- ResumeBuilder/src/components/UserResume.jsx
+++ ResumeBuilder/src/components/UserResume.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React,{ useRef } from "react";
+import html2pdf from "html2pdf.js";
 
 function UserResume({ userDetails}) {
+
+    const resumeRef = useRef(null);
+    const handleDownload = () => {
+        console.log("Download")
+        const options = {
+            margin: [0, 0, 20, 0], 
+            filename: 'cv.pdf', 
+            image: { type: 'jpeg', quality: 0.98 }, 
+            html2canvas: { scale: 2 }, 
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }, 
+          };
+        html2pdf().from(resumeRef.current).set(options).save("resume.pdf");
+      };
+
+      
   return (
-    <div className="p-10 w-full">
-    <div className="bd p-10 bg-[#F1F0E8]">
+    <div className="p-10 w-full" id="user-resume-2" ref={resumeRef}>
+    <div className="p-10 bg-[#F1F0E8]">
       <p className="text-center text-3xl font-bold">{userDetails.name}</p>
       <div className="flex justify-center">
         <p className="w-full text-right px-2">
@@ -34,6 +50,7 @@ function UserResume({ userDetails}) {
         <div className="resume-heading">Skills : </div>
       </div>
     </div>
+    <button onClick={handleDownload}>Download Resume</button>
     </div>
   );
 }
--- ResumeBuilder/src/pages/Home.jsx
+++ ResumeBuilder/src/pages/Home.jsx
@@ -1,33 +1,35 @@
-import React, {useState} from 'react'
-import UserInput from '../components/UserInput'
-import UserResume from '../components/UserResume'
+import React, { useState } from "react";
+import UserInput from "../components/UserInput";
+import UserResume from "../components/UserResume";
 
 function Home() {
-    const [userDetails, setUserDetails] = useState({
-        name: '',
-        email: '',
-        phoneno: '',
-        description: '',
-    })
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    phoneno: "",
+    description: "",
+  });
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserDetails({
-           ...userDetails,
-            [name]: value
-        })
-    }
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserDetails({
+      ...userDetails,
+      [name]: value,
+    });
+  };
 
-
-    return (
-        <div>
-            <p className='p-2 text-2xl font-bold ml-5'>Resume Builder</p>
-            <div className='lg:flex'>
-                <UserInput userDetails={userDetails} onInputChange={handleInputChange} />
-                <UserResume userDetails={userDetails} />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <p className="p-2 text-2xl font-bold ml-5">Resume Builder</p>
+      <div className="lg:flex">
+        <UserInput
+          userDetails={userDetails}
+          onInputChange={handleInputChange}
+        />
+        <UserResume userDetails={userDetails} />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
