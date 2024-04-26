dad7b973649b63376ffbbebc35c4b2d20ecd4dbb
Athul Bhandary <athulbhandary28@gmail.com>
2024-04-26 17:24:01 +0530

    Changed popUp auth to redirection

--- ResumeBuilder/src/firebase/auth.js
+++ ResumeBuilder/src/firebase/auth.js
@@ -7,6 +7,7 @@ import {
   updatePassword,
   signInWithPopup,
   GoogleAuthProvider,
+  signInWithRedirect
 } from "firebase/auth";
 
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
@@ -19,7 +20,7 @@ export const doSignInWithEmailAndPassword = (email, password) => {
 
 export const doSignInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
-  const result = await signInWithPopup(auth, provider);
+  const result = await signInWithRedirect(auth, provider);
   const user = result.user;
 
   // add user to firestore
